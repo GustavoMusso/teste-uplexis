b0ab86927a4459cf81cb47a893a9426cd45aa181
Gustavo <gustavomussolino@hotmail.com>
2019-07-20 16:07:16 -0300

    Iniciando metodo de busca de artigos

--- app/Services/Capturas.php
+++ app/Services/Capturas.php
@@ -2,23 +2,7 @@
 
 namespace App\Services;
 
-use GuzzleHttp\Client;
-use Illuminate\Support\Facades\Auth;
-
 class Capturas
 {
-    private $servicoArtigos;
-    private $guzzle;
-
-    public function __construct()
-    {
-        $this->guzzle = new Client();
-        $this->servicoArtigos = new Artigos();
-    }
-
-    public function buscaArtigos(array $parametro)
-    {
-
-    }
     
 }
