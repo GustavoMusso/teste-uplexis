c6d2a864f0686f4a9f7b9b5ed3d536c4de6370b8
Gustavo <gustavomussolino@hotmail.com>
2019-07-19 22:56:04 -0300

    Finalizando Artigos

---
+++ app/Artigo.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Artigo extends Model
+{
+    protected $table = 'artigos';
+
+    protected $fillable = [
+        'id_usuario', 'titulo', 'link',
+    ];
+
+    public $timestamps = false;
+
+    public function user()
+    {
+        return $this->hasMany(User::class, 'id', 'id_usuario');
+    }
+}
